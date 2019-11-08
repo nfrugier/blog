c9afe59e186d27a7dd2a0911211f8c865ba61dd5
nfrugier <nicolas.frugier2@gmail.com>
2019-11-08 16:22:12 +0100

    fix js textarea autoresize

--- assets/js/autoarea.js
+++ assets/js/autoarea.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     var textarea = document.querySelectorAll("textarea");
-    textarea[0].addEventListener("keypress", () => {
-        if(textarea[0].scrollTop != 0){
-            textarea[0].style.height = textarea[0].scrollHeight + "px";
-        }
-    }, false)
+    for (var i=0; i < textarea.length ;i++){
+        textarea[i].addEventListener("keypress", () => {
+            if(textarea[i].scrollTop != 0){
+                textarea[i].style.height = textarea[i].scrollHeight + "px";
+            }
+        }, false)
+    }
 }, false);
